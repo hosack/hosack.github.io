b7a2fcd41dbfa3230c8229582d67b94b34dec372
mhk <mhk@ableton.com>
2024-03-05 22:53:24 +0100

    Adding svelte config base path from docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,41 @@
-import adapter from '@sveltejs/adapter-static'
-import { mdsvex } from 'mdsvex'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
+import adapter from "@sveltejs/adapter-static";
+import { mdsvex } from "mdsvex";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
-	extensions: ['.svelte', '.md'],
+  // Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
+  extensions: [".svelte", ".md"],
 
-	preprocess: [
-		mdsvex({
-			// The default mdsvex extension is .svx; this overrides that.
-			extensions: ['.md'],
+  preprocess: [
+    mdsvex({
+      // The default mdsvex extension is .svx; this overrides that.
+      extensions: [".md"],
 
-			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-			rehypePlugins: [
-				rehypeSlug,
-				rehypeAutolinkHeadings,
-			],
-		}),
-	],
+      // Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    }),
+  ],
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
     prerender: {
       entries: [
-        '*',
-        '/api/posts/page/*',
-        '/blog/category/*/page/',
-        '/blog/category/*/page/*',
-        '/blog/category/page/',
-        '/blog/category/page/*',
-        '/blog/page/',
-        '/blog/page/*',
-      ]
-    }
-	}
+        "*",
+        "/api/posts/page/*",
+        "/blog/category/*/page/",
+        "/blog/category/*/page/*",
+        "/blog/category/page/",
+        "/blog/category/page/*",
+        "/blog/page/",
+        "/blog/page/*",
+      ],
+    },
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    },
+  },
 };
 
 export default config;
